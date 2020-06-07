f2ac77a70bc97bd2fdea05c5e73eab20f35e414d
Dabolo <976264709@qq.com>
2020-06-07 12:37:54 +0800

    /admin controller start

---
+++ Server/src/main/java/com/backend/server/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.backend.server.controller;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class AdminController {
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+}
